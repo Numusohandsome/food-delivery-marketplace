d97ebc5259b63d380a31bacb6ce19b673ecacf88
Sala <salamakhsetov18@gmail.com>
2026-05-11 15:05:39 +0500

    db: add core tables migration

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql+psycopg2://postgres:postgres@localhost:5432/food_delivery"
+DATABASE_URL = "postgresql+psycopg2://food_user:food_password@localhost:5432/food_delivery"
 
 engine = create_engine(DATABASE_URL)
 
